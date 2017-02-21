b23f505eed8676b87c94b228809a99d8c7bb44c6
Chris <orthagonal1@gmail.com>
2017-02-21 16:12:45 -0600

    add 2nd test case

--- test/test.execute.js
+++ test/test.execute.js
@@ -24,4 +24,24 @@ describe('str2fn.execute', () => {
       }
     );
   });
+  it('can execute a method as it might appear in hapi-views, etc', (done) => {
+    str2fn.execute(
+      'method.name("blah", "blah2")',
+      {
+        method: {
+          name: (var1, var2, methodDone) => {
+            chai.expect(var1).to.equal('blah');
+            chai.expect(var2).to.equal('blah2');
+            methodDone(null, var1 + var2);
+          }
+        }
+      },
+      {},
+      (err, results) => {
+        chai.expect(err).to.equal(null);
+        chai.expect(results).to.equal('blahblah2');
+        done();
+      }
+    );
+  });
 });
