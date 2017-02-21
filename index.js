ce33ff6097906e90be634f3b1a9b6098723d8576
Chris <orthagonal1@gmail.com>
2017-02-21 16:12:54 -0600

    execute

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const restOf = require('lodash.tail');
+const vm = require('vm');
 // str2fn(hapi.methods, 'some.name')(arg1, arg2)
 const get = (str, obj, fallback) => {
   if (typeof str === 'function') {
@@ -7,7 +8,7 @@ const get = (str, obj, fallback) => {
   }
   const keys = str.split('.');
   const call = keys.reduce((current, nextKey) => {
-    return current ? current[nextKey] : undefined
+    return current ? current[nextKey] : undefined;
   }, obj);
   if (call) {
     return call;
@@ -21,5 +22,18 @@ const get = (str, obj, fallback) => {
   throw new Error(`Method ${str} does not exist`);
 };
 const str2fn = (obj, str, fallback) => get(str, obj, fallback);
+
+const execute = (callString, obj, context, executeDone) => {
+  const split = callString.split('(');
+  const funcName = split[0];
+  const paramString = restOf(split).join('(').slice(0, -1);
+  context.str2fn = str2fn;
+  context.str2fn_executeDone = executeDone;
+  context.str2fn_obj = obj;
+  const scriptToExecute = `str2fn(str2fn_obj, '${funcName}')(${paramString}, str2fn_executeDone);`;
+  vm.runInNewContext(scriptToExecute, context);
+};
+
 str2fn.get = get;
+str2fn.execute = execute;
 module.exports = str2fn;
